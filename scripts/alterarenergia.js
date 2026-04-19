c55b96bbe36660ce09f04a958e327dd8ec9a70b8
Donovan <donovanperello@paucasesnovescifp.cat>
2026-04-19 12:04:26 +0200

    Refactor code structure for improved readability and maintainability

--- scripts/alterarenergia.js
+++ scripts/alterarenergia.js
@@ -1,6 +1,31 @@
 const toggleBoton = document.getElementById("theme-toggle");
-
+let contador = 0;
+const imagenSecreta = document.getElementById("easter-egg");
 toggleBoton.addEventListener('click', () => {
     document.body.classList.toggle('light-theme');
+    contador++;
+
+    
+    if (contador === 15) {
+        activarEasterEgg();
+    }
+
+    
 });
+
+
+function activarEasterEgg(){
+
+    imagenSecreta.style.display = "block";
+
+    setTimeout(() => {
+        imagenSecreta.style.display = "none";
+        contadorClicks = 0; 
+    }, 5000);
+
+   
+}
+
+
+
 
