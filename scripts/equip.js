3afb44029988bc170cbb29893fd1b1e69a3b130e
AdrianMatasMunoz <adrianmatas@paucasesnovescifp.cat>
2026-04-24 09:23:07 +0200

    Fix: Animació de equip.html millorada.

--- scripts/equip.js
+++ scripts/equip.js
@@ -1,54 +1,42 @@
 let cur_developer = 0;
 const SCROLL_ROTATE_HEIGHT = 300;
-const SCROLL_OPACITY_RANGE = 100;
 
-function getTransitionValue() {
-    return (SCROLL_ROTATE_HEIGHT - window.scrollY)/(SCROLL_OPACITY_RANGE);
-}
-
-function clamp(v, min, max) {
-    return Math.min(Math.max(v, min), max);
-}
-
-function update() {
+function updateResponsive() {
     for(let i = 0; i < 3; i++) {
         document.getElementById(`container-${i}`).style.display =
             (i != cur_developer && window.innerWidth <= 1400 ? "none" : "");
     }
-
-    console.log(scrollY);
-
-    document.getElementById("gamedev-section").style =
-        `transform: rotate(${window.scrollY < SCROLL_ROTATE_HEIGHT ? "0deg" : "180deg"}) translate(${clamp(Math.abs(50/getTransitionValue()), 0, 2000) - 20}px); 
-         opacity: ${getCurrentOpacity()};`;
-    document.getElementById("webdev-section").style =
-        `transform: rotate(${window.scrollY < SCROLL_ROTATE_HEIGHT ? "180deg" : "0deg"}) translate(${clamp(Math.abs(50/getTransitionValue()), 0, 2000) - 20}px);
-         opacity: ${getCurrentOpacity()};`;
 }
 
-function getCurrentOpacity() {
-    return Math.min(Math.abs(getTransitionValue()), 1);
+function updateScrollInversion() {
+    if(scrollY < SCROLL_ROTATE_HEIGHT) {
+        document.getElementById("gamedev-section").classList.remove("reverse");
+        document.getElementById("webdev-section").classList.add("reverse");
+    } else {
+        document.getElementById("gamedev-section").classList.add("reverse");
+        document.getElementById("webdev-section").classList.remove("reverse");
+    }
 }
 
 document.getElementById("previous-dev").addEventListener("click", (ev) => {
     cur_developer--;
     if(cur_developer < 0) cur_developer = 2;
 
-    update();
+    updateResponsive();
 });
 
 document.getElementById("next-dev").addEventListener("click", (ev) => {
     cur_developer++;
     if(cur_developer >= 3) cur_developer = 0;
-    update();
+    updateResponsive();
 });
 
 window.addEventListener("resize", (ev) => {
-    update();
+    updateResponsive();
 });
 
 window.addEventListener("scroll", (ev) => {
-    update();
+    updateScrollInversion();
 });
 
-update();
+updateResponsive();
